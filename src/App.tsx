201dcabda886576ef6202eb3ef8c7f5025883b83
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2024-05-03 18:06:26 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Listing } from './components/Listing.tsx'
 import data from './data/etsy.json'
 
 function App() {
-  const listingItems = data.filter(el => el.state === 'active')
+  const listingItems = data.filter(el => el.state == 'active')
   return (
     <>
       <Listing items={listingItems}/>
